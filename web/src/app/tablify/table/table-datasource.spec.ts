f2511e4c712cb491c90ff74cea06c346fe2003e6
thomas <me@thomasanderson.cloud>
2022-04-18 00:41:24 -0400

    improve some table-datasource tests

---
+++ web/src/app/tablify/table/table-datasource.spec.ts
@@ -0,0 +1,81 @@
+import { ApiService } from 'src/app/services/api.service';
+import { MockApiService, MOCK_SCRIPTS } from 'src/app/services/api.service.mock';
+import { TableDataSource } from './table-datasource';
+
+
+describe('TableDataSource', () => {
+  let obj: TableDataSource;
+
+  beforeEach(() => {
+    obj = new TableDataSource()
+  });
+
+  it('should compile', () => {
+    expect(obj).toBeTruthy();
+  });
+
+   it('updateData() should set this.data and call updated.next()', () => {
+    const testdata = {data: 'test'} as any
+    spyOn(obj.updated, 'next')
+    obj.updateData(testdata)
+    expect(obj.data).toBe(testdata)
+    expect(obj.updated.next).toHaveBeenCalledWith(true)
+  })
+
+  it('disconnect() should do nothing', () => {
+    spyOn(obj, 'disconnect').and.callThrough()
+    obj.disconnect()
+    expect(obj.disconnect).toHaveBeenCalled()
+  })
+
+  it('sortIsActive() should return true if sort.active ', () => {
+    obj.sort = {active: ''} as any
+    expect(obj.sortIsActive()).toBeFalse()
+    obj.sort.active = 'active'
+    expect(obj.sortIsActive()).toBeTrue()
+  })
+
+  it('getSortedData() should immediately return data as given if !sortIsActive', () => {
+    const testdata = [{data: 'test'}] as any
+    spyOn(obj, 'sortIsActive').and.returnValue(false)
+    spyOn(obj, 'localeSortInPlace')
+    const result = obj.getSortedData(testdata)
+    expect(obj.localeSortInPlace).not.toHaveBeenCalled()
+    expect(result).toBe(testdata)
+  })
+
+  it('getSortedData() should call localeSortInPlace if sortIsActive and not data.reverse if !isDescending()', () => {
+    const testdata = [{data: 'test'}] as any
+    const testkey = 'key1'
+    spyOn(obj, 'sortIsActive').and.returnValue(true)
+    spyOn(obj, 'isDescending').and.returnValue(false)
+    spyOn(testdata, 'reverse')
+    spyOn(obj, 'localeSortInPlace')
+    obj.sort = {active: testkey} as any
+    const result = obj.getSortedData(testdata)
+    expect(obj.localeSortInPlace).toHaveBeenCalledWith(testdata, testkey)
+    expect(testdata.reverse).not.toHaveBeenCalled()
+    expect(result).toBe(testdata)
+  })
+
+  it('getSortedData() should call localeSortInPlace if sortIsActive and data.reverse if isDescending()', () => {
+    const testdata = [{data: 'test'}] as any
+    const testkey = 'key1'
+    spyOn(obj, 'sortIsActive').and.returnValue(true)
+    spyOn(obj, 'isDescending').and.returnValue(true)
+    spyOn(testdata, 'reverse')
+    spyOn(obj, 'localeSortInPlace')
+    obj.sort = {active: testkey} as any
+    const result = obj.getSortedData(testdata)
+    expect(obj.localeSortInPlace).toHaveBeenCalledWith(testdata, testkey)
+    expect(testdata.reverse).toHaveBeenCalled()
+    expect(result).toBe(testdata)
+  })
+  
+  it('isDescending() should return true only if sort.direction === desc', () => {
+    obj.sort = {direction: 'wrong'} as any
+    expect(obj.isDescending()).toBeFalse()
+    obj.sort.direction = 'desc'
+    expect(obj.isDescending()).toBeTrue()
+  })
+})
--- web/src/app/tablify/table/table-datasource.ts
+++ web/src/app/tablify/table/table-datasource.ts
@@ -22,15 +22,9 @@ export interface TableScriptItem {
  */
 export class TableDataSource extends DataSource<TableScriptItem> {
   data: any[] = [];
-  paginator: MatPaginator | undefined;
-  sort: MatSort | undefined;
+  paginator!: MatPaginator;
+  sort!: MatSort;
   updated = new Subject();
-
-
-
-  constructor() {
-    super();
-  }
 
   updateData(data: any[]) {
     this.data = data
@@ -43,17 +37,13 @@ export class TableDataSource extends DataSource<TableScriptItem> {
    * @returns A stream of the items to be rendered.
    */
   connect(): Observable<TableScriptItem[]> {
-    if (this.paginator && this.sort) {
-      // Combine everything that affects the rendered data into one update
-      // stream for the data-table to consume.
-      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange, this.updated)
-        .pipe(map(() => {
-          let data = this.data || []
-          return this.getPagedData(this.getSortedData([...data]));
-        }));
-    } else {
-      throw Error('Please set the paginator and sort on the data source before connecting.');
-    }
+    // Combine everything that affects the rendered data into one update
+    // stream for the data-table to consume.
+    return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange, this.updated)
+      .pipe(map(() => {
+        let data = this.data || []
+        return this.getPagedData(this.getSortedData([...data]));
+      }));
   }
 
   /**
@@ -66,39 +56,44 @@ export class TableDataSource extends DataSource<TableScriptItem> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: TableScriptItem[]): TableScriptItem[] {
-    if (this.paginator) {
-      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-      return data.splice(startIndex, this.paginator.pageSize);
-    } else {
-      return data;
-    }
+  getPagedData(data: TableScriptItem[]): TableScriptItem[] {
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    return data.splice(startIndex, this.paginator.pageSize);
+  }
+
+  sortIsActive(): boolean {
+    return !!(this.sort.active && this.sort.direction !== '')
   }
 
   /**
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: any[]): any[] {
-    if (!this.sort || !this.sort.active || this.sort.direction === '') {
+  getSortedData(data: any[]): any[] {
+    if (!this.sortIsActive()) {
       // Don't sort
       return data;
     }
 
     const sortKey = this.sort.active
-    data.sort((a, b) => {
-      return compareKeys(sortKey, a, b)
-    });
-    if (this.sort?.direction === 'desc') {
+    this.localeSortInPlace(data, sortKey)
+    if (this.isDescending()) {
       data.reverse()
     }
     return data
   }
-}
 
-function compareKeys(key: string, a: any, b: any) {
-  const aVal = String(a[key])
-  const bVal = String(b[key])
+  localeSortInPlace(data: any[], key: string) {
+    data.sort((a, b) => {
+      return this.localeCompare(a[key], b[key])
+    });
+  }
 
-  return aVal.localeCompare(bVal, undefined, { numeric: true })
+  localeCompare(a: any, b: any) {
+    return String(a).localeCompare(String(b), undefined, { numeric: true })
+  }
+
+  isDescending() {
+    return (this.sort.direction === 'desc')
+  }
 }
