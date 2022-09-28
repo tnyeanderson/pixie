9b0cd148c137dc03a4d185ec4cf30ddf32a33397
thomas <me@thomasanderson.cloud>
2022-09-28 03:04:15 -0400

    wip remove scripts/images/cloudconfigs components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -13,49 +13,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormifyModule } from './formify/formify.module';
 import { AddDeviceComponent } from './forms/add-device/add-device.component';
-import { AddImageComponent } from './forms/add-image/add-image.component';
-import { AddScriptComponent } from './forms/add-script/add-script.component';
+import { AddFileComponent } from './forms/add-file/add-file.component';
 import { EditDeviceComponent } from './forms/edit-device/edit-device.component';
-import { EditImageComponent } from './forms/edit-image/edit-image.component';
-import { EditScriptComponent } from './forms/edit-script/edit-script.component';
+import { EditFileComponent } from './forms/edit-file/edit-file.component';
+import { ReloadButtonModule } from './fragments/reload-button/reload-button.module';
 import { DevicesListComponent } from './lists/devices-list/devices-list.component';
-import { ImagesListComponent } from './lists/images-list/images-list.component';
+import { FilesListComponent } from './lists/files-list/files-list.component';
 import { LogsListComponent } from './lists/logs-list/logs-list.component';
-import { ScriptsListComponent } from './lists/scripts-list/scripts-list.component';
 import { NavModule } from './nav/nav.module';
 import { ApiService } from './services/api.service';
 import { ConfirmService } from './services/confirm/confirm.service';
 import { ConfirmationDialogComponent } from './services/confirm/confirmation-dialog/confirmation-dialog.component';
 import { TablifyModule } from './tablify/tablify.module';
-import { ReloadButtonModule } from './fragments/reload-button/reload-button.module';
-import { CloudConfigsListComponent } from './lists/cloud-configs-list/cloud-configs-list.component';
-import { AddCloudConfigComponent } from './forms/add-cloud-config/add-cloud-config.component';
-import { EditCloudConfigComponent } from './forms/edit-cloud-config/edit-cloud-config.component';
-import { AddFileComponent } from './forms/add-file/add-file.component';
-import { EditFileComponent } from './forms/edit-file/edit-file.component';
-import { FilesListComponent } from './lists/files-list/files-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FilesListComponent,
-    ImagesListComponent,
-    ScriptsListComponent,
     DevicesListComponent,
     AddFileComponent,
     EditFileComponent,
-    AddImageComponent,
-    AddScriptComponent,
-    EditScriptComponent,
     ConfirmationDialogComponent,
     AddDeviceComponent,
     EditDeviceComponent,
     LogsListComponent,
-    EditImageComponent,
-    CloudConfigsListComponent,
-    AddCloudConfigComponent,
-    EditCloudConfigComponent,
   ],
   imports: [
     BrowserModule,
--- web/src/app/tablify/services/data-source.service.ts
+++ web/src/app/tablify/services/data-source.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CloudConfigsTableDataSource } from 'src/app/lists/cloud-configs-list/cloud-configs-table-datasource';
 import { DevicesTableDataSource } from 'src/app/lists/devices-list/devices-table-datasource';
 import { FilesTableDataSource } from 'src/app/lists/files-list/files-table-datasource';
-import { ImagesTableDataSource } from 'src/app/lists/images-list/images-table-datasource';
 import { LogsTableDataSource } from 'src/app/lists/logs-list/logs-table-datasource';
-import { ScriptsTableDataSource } from 'src/app/lists/scripts-list/scripts-table-datasource';
 
 @Injectable({
   providedIn: 'root'
@@ -15,24 +12,12 @@ export class DataSourceService {
     return new DevicesTableDataSource()
   }
 
-  createCloudConfigsTableDataSource() {
-    return new CloudConfigsTableDataSource()
-  }
-
-  createImagesTableDataSource() {
-    return new ImagesTableDataSource()
-  }
-
   createFilesTableDataSource() {
     return new FilesTableDataSource()
   }
 
   createLogsTableDataSource() {
     return new LogsTableDataSource()
-  }
-
-  createScriptsTableDataSource() {
-    return new ScriptsTableDataSource()
   }
 
   constructor() { }
