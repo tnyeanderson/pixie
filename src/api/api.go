af807fd10ea989cf8c4605cf791f44a1d2b0f386
thomas <me@thomasanderson.cloud>
2023-10-23 01:03:14 -0400

    feat!: remove angular site

--- src/api/api.go
+++ src/api/api.go
@@ -8,12 +8,6 @@ func NewRouter() *gin.Engine {
 	// Set up gin
 	r := gin.Default()
 
-	//// Redirect to angular site from site root
-	//r.GET("/", func(c *gin.Context) {
-	//	location := url.URL{Path: "/app"}
-	//	c.Redirect(http.StatusFound, location.RequestURI())
-	//})
-
 	// API
 	// Default base path: /api/v1
 	v1 := r.Group("/api/v1")
@@ -31,15 +25,6 @@ func NewRouter() *gin.Engine {
 
 	// Renderer
 	r.GET("/render/:mac/*path", GetConfig, StaticHandler)
-
-	//// Angular site
-	//r.Static("/app", config.Pixie.Paths.WebRoot)
-	//r.NoRoute(func(c *gin.Context) {
-	//	fmt.Println("Route not found for:", c.Request.RequestURI)
-	//	if strings.HasPrefix(c.Request.RequestURI, "/app") {
-	//		c.File(filepath.Join(config.Pixie.Paths.WebRoot, "index.html"))
-	//	}
-	//})
 
 	return r
 }
